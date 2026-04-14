99590cb68c889c49389835fdf245e0f7c6148f03
hagernofal <hagernofal8@gmail.com>
2026-04-15 00:31:40 +0200

    update layout of reception to display real number of pations in queue

--- src/app/components/layouts/receptionist-layout/receptionist-layout.component.ts
+++ src/app/components/layouts/receptionist-layout/receptionist-layout.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
+import { Reception } from '../../../services/reception';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-receptionist-layout',
   standalone: true,
   imports: [
+    CommonModule,
     RouterLink,
     RouterLinkActive,
     RouterOutlet,
@@ -16,4 +19,27 @@ import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
   templateUrl: './receptionist-layout.component.html',
   styleUrls: ['./receptionist-layout.component.css'],
 })
-export class ReceptionistLayoutComponent {}
+export class ReceptionistLayoutComponent implements OnInit {
+  private receptionService = inject(Reception);
+private cdr = inject(ChangeDetectorRef);
+  todayQueueCount: number = 0;
+
+  ngOnInit(): void {
+    this.loadQueueCount();
+    this.cdr.detectChanges();
+  }
+
+  loadQueueCount(): void {
+    this.receptionService.getTodayQueue().subscribe({
+      next: (data) => {
+        this.todayQueueCount = data.length;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('Error fetching count:', err);
+        this.todayQueueCount = 0;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+}
